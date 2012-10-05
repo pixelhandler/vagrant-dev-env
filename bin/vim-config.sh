131d9294cc5577e38bf7975f667d049bf71d59df
Bill Heaton <pixelhandler@gmail.com>
2012-10-05 14:48:51 -0700

    update bin/vim-config.sh

--- bin/vim-config.sh
+++ bin/vim-config.sh
@@ -2,21 +2,19 @@
 
 # Vim config - see: https://github.com/pixelhandler/vim-config
 
-if [ -d /home/vagrant/.vim ]
-then
-  echo 'vim config already setup. '
+if [ -d /home/vagrant/.vim ]; then echo 'vim config already setup. '
 else
-  git clone git://github.com/pixelhandler/vim-config.git ./.vim
-  cd /home/vagrant/.vim
+  mkdir /home/vagrant/.vim && cd /home/vagrant.vim
+  mkdir _backup _temp
+  git clone git://github.com/pixelhandler/vim-config.git .
   git submodule init
   git submodule update
   git submodule foreach git submodule init
   git submodule foreach git submodule update
-  cd /home/vagrant/.vim/bundle/command-t/
-  rake make 
-  mkdir /home/vagrant/.vim/_backup
-  mkdir /home/vagrant/.vim/_temp
-  ln -s /home/vagrant/.vim/.vimrc /home/vagrant/.vimrc
+  cd bundle/command-t/
+  rake make
+  cd ~
+  ln -s .vim/.vimrc ./.vimrc
   echo 'vim config setup done. '
 fi
 
