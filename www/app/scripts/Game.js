dcd504be665949e062794a01d069bf90e9c5354e
Bill Heaton <pixelhandler@gmail.com>
2012-10-23 18:16:25 +0000

    add failing test for gutter game, and stub roll and score methods

--- www/app/scripts/Game.js
+++ www/app/scripts/Game.js
@@ -1,5 +1,15 @@
 define('game', function () {
     var Game = function () {};
 
+    Game.prototype.roll = function (pins) {
+        if (typeof pins !== 'number') {
+            throw new Error('Game.role() expects `pins` argument to be a number');
+        }
+    };
+
+    Game.prototype.score = function () {
+        return -1;
+    };
+
     return Game;
 });
--- www/test/spec/BowlingGame.js
+++ www/test/spec/BowlingGame.js
@@ -6,7 +6,13 @@ describe("Bowling Game Kata", function () {
 
         it("should score zero", function () {
             require(['game'], function (Game) {
-                var game = new Game();
+                var game = new Game(),
+                    i = 0;
+
+                for (i; i < 20; i ++) {
+                    game.roll(0);
+                }
+                expect(game.score()).to.equal(0);
             });
         });
 
