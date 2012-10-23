739c41aece23a39becbe283e6bd9cee9ee31d204
Bill Heaton <pixelhandler@gmail.com>
2012-10-23 17:43:36 +0000

    added failing test for gutter game

---
+++ www/test/spec/BowlingGame.js
@@ -0,0 +1,13 @@
+// Bowling Game specs
+
+describe("Bowling Game Kata", function () {
+
+    describe("Gutter Game", function () {
+
+        it("should score zero", function () {
+            var game = new Game();
+        });
+
+    });
+
+});
