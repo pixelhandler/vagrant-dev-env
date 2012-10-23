80e0fc428c0900d45c87a6f3e341951d19667c4c
Bill Heaton <pixelhandler@gmail.com>
2012-10-23 18:29:30 +0000

    add passing test for game with 20 rolls each hitting 1 pin

--- www/test/spec/BowlingGame.js
+++ www/test/spec/BowlingGame.js
@@ -6,13 +6,27 @@ describe("Bowling Game Kata", function () {
     describe("Gutter Game", function () {
 
         it("should score zero", function () {
-                var game = new Game(),
-                    i = 0;
+            var game = new Game(),
+                i = 0;
 
-                for (i; i < 20; i ++) {
-                    game.roll(0);
-                }
-                expect(game.score()).to.equal(0);
+            for (i; i < 20; i ++) {
+                game.roll(0);
+            }
+            expect(game.score()).to.equal(0);
+        });
+
+    });
+
+    describe("Game with every roll only hitting one pin", function () {
+
+        it("should score 20 given each roll hits 1 pin", function () {
+            var game = new Game(),
+                i = 0;
+
+            for (i; i < 20; i ++) {
+                game.roll(1);
+            }
+            expect(game.score()).to.equal(20);
         });
 
     });
