cfa24aacd9b2ea6b97ff12692dfcf1158635afb8
Bill Heaton <pixelhandler@gmail.com>
2012-10-23 20:03:21 +0000

    pass tests for rolling and scoring spares

--- www/app/scripts/Game.js
+++ www/app/scripts/Game.js
@@ -1,21 +1,53 @@
 define('game', function () {
     var Game = function () {
-        this._score = 0;
+        this._currentRoll = 0;
+        this._rolls = [];
+        this._spares = [];
     };
 
-    // TODO design is wrong, responsibilities are missplaced...
-
-    // TODO roll should not calculate score
     Game.prototype.roll = function (pins) {
         if (typeof pins !== 'number') {
             throw new Error('Game.role() expects `pins` argument to be a number');
         }
-        this._score += pins;
+        this._rolls[this._currentRoll++] = pins;
     };
 
-    // TODO score is not actually calculating value
     Game.prototype.score = function () {
-        return this._score;
+        var score = 0, frameIdx = 0;
+
+        for (frameIdx; frameIdx < this._rolls.length; frameIdx++) {
+            if (this._isSpare(frameIdx)) {
+                score += 10 + this._rolls[frameIdx + 2];
+                frameIdx ++;
+            } else {
+                score += this._rolls[frameIdx];
+            }
+        }
+
+        return score;
+    };
+
+    Game.prototype._isSpare = function (frameIdx) {
+        var isSpare = (this._rolls[frameIdx] + this._rolls[frameIdx + 1] === 10),
+            isLastRollSpare,
+            i = 0;
+
+        if (isSpare) {
+            if (this._rolls.length) {
+                for (i; i < this._spares.length; i++) {
+                    if (this._spares[i] === frameIdx - 1) {
+                        isLastRollSpare = true;
+                    }
+                }
+            }
+            if (!isLastRollSpare) {
+                this._spares.push(frameIdx);
+            } else {
+                isSpare = false;
+            }
+        }
+
+        return isSpare;
     };
 
     return Game;
--- www/test/spec/BowlingGame.js
+++ www/test/spec/BowlingGame.js
@@ -11,6 +11,11 @@ describe("Bowling Game Kata", function () {
         }
     }
 
+    function rollSpare() {
+        this.roll(5);
+        this.roll(5);
+    }
+
     beforeEach(function () {
         this.game = new Game();
     });
@@ -33,14 +38,23 @@ describe("Bowling Game Kata", function () {
 
     });
 
-    describe.skip("Game with one spare", function () {
+    describe("Game with one spare", function () {
 
         it("should score 20 given the first 3 rolls hit 5 pins", function () {
-            rollMany.call(this.game, 2, 5); // roll spare
+            rollSpare.call(this.game)
             this.game.roll(5);
             rollMany.call(this.game, 17, 0);
             expect(this.game.score()).to.equal(20);
         });
+        
+        it("should score 19 given a spare and 2 following rolls of 3", function () {
+            rollSpare.call(this.game)
+            this.game.roll(3);
+            this.game.roll(3);
+            rollMany.call(this.game, 16, 0);
+            expect(this.game.score()).to.equal(19);
+        });
+
 
     });
 
