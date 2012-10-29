3f92bcc27e6081f4bfde7becfc911c57385f11c8
Bill Heaton <pixelhandler@gmail.com>
2012-10-29 22:07:38 +0000

    add bonus roll method to help score game in 10th frame

--- www/Gruntfile.js
+++ www/Gruntfile.js
@@ -68,7 +68,8 @@ module.exports = function( grunt ) {
           'app/*.html',
           'app/styles/**/*.css',
           'app/scripts/**/*.js',
-          'app/images/**/*'
+          'app/images/**/*',
+          'test/**/*'
         ],
         tasks: 'reload'
       }
