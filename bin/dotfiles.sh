ce80d0bcf3c5e0b7c705727d3c3f2b12f803eeaf
Bill Heaton <pixelhandler@gmail.com>
2013-05-06 08:51:49 -0700

    Update dotfiles.sh

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -1,53 +1,45 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-DOTFILES='/vagrant/dotfiles' 
+# use like
+# `vagrant ssh`
+# `/vagrant/bin/bootstrap.sh`
 
-if [ ! -d $HOME/.ssh ];
-then
-    echo "Creating $HOME/.ssh directory"
-    mkdir $HOME/.ssh
+# Or for dry run... add option -d or --dry-run
+# `/vagrant/bin/bootstrap.sh -d` 
+# `/vagrant/bin/bootstrap.sh --dry-run` 
 
-    echo "Copying dotfiles/.ssh/config"
-    cp $DOTFILES/.ssh/config $HOME/.ssh/config
+# To skip prompt and force execution... add option -f or --force
+# `/vagrant/bin/bootstrap.sh -f` 
+# `/vagrant/bin/bootstrap.sh --force` 
 
-	# Copy contents of dotfiles/.bashrc to ~/.bashrc
-    echo "Copying dotfiles/.bashrc into ~/.bashrc"
-    cat $DOTFILES/.bashrc >> $HOME/.bashrc
+# Note copy your keys id_rsa and id_rsa.pub to /dotfiles/.ssh/ 
+# this will copy the keys into your vagrant box
+# e.g. `cp ~/.ssh/id_rsa* /your_path_to.../dotfiles/.ssh/`
+
+cd "$(dirname "${BASH_SOURCE}")"
+
+function doIt() {
+    if [ ! -d $HOME/.ssh ]; then
+        echo "Creating $HOME/.ssh directory"
+        mkdir $HOME/.ssh
+    fi
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -Pav /vagrant/dotfiles/ ~
+}
+function dryRun() {
+    echo "Dry Run..."
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -Pavn /vagrant/dotfiles/ ~
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt
+elif [ "$1" == "--dry-run" -o "$1" == "-d" ]; then
+    dryRun
+else
+    read -p "This may overwrite existing files in your /home/vagrant/ directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
 fi
-
-# should not need this precise64 box provisions with a .profile which includes .bashrc
-# if [ ! -f $HOME/.bash_profile ];
-# then
-#     echo "Copying dotfiles/.bash_profile"
-#     cp $DOTFILES/.bash_profile $HOME/.bash_profile
-# fi
-
-if [ ! -f $HOME/.ackrc ];
-then
-    echo "Copying dotfiles/.ackrc"
-    cp $DOTFILES/.ackrc $HOME/.ackrc
-fi
-
-if [ ! -f $HOME/.gitconfig ];
-then
-    echo "Copying dotfiles/.gitconfig"
-    cp $DOTFILES/.gitconfig $HOME/.gitconfig
-fi
-
-if [ ! -f $HOME/.jshintrc ];
-then
-    echo "Copying dotfiles/.jshintrc"
-    cp $DOTFILES/.jshintrc $HOME/.jshintrc
-fi
-
-if [ ! -f $HOME/.jslintrc ];
-then
-    echo "Copying dotfiles/.jslintrc"
-    cp $DOTFILES/.jslintrc $HOME/.jslintrc
-fi
-
-if [ ! -f $HOME/.my.cnf ];
-then
-    echo "Copying dotfiles/.my.cnf"
-    cp $DOTFILES/.my.cnf $HOME/.my.cnf
-fi
+unset doIt
+unset dryRun
+source ~/.bash_profile
